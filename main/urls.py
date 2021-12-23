a75a76746a0b74b886153e1428e70406363abd9c
James Deschard <jamesdeschard@jamess-air.home>
2021-12-23 21:15:53 +0100

    fixed stretching issue

--- main/urls.py
+++ main/urls.py
@@ -7,12 +7,12 @@ def get_name(_class):
 urlpatterns = [
     path('', HomePage.as_view(), name=get_name(HomePage)),
     re_path(
-        r'(?P<width>[0-9]+)/(?P<height>[0-9]+)/', 
+        r'(?P<width>[0-9]+)/(?P<height>[0-9]+)', 
         ImageView.as_view(), 
         name=get_name(ImageView)
     ),
     re_path(
-        r'(?P<width>[0-9]+)/(?P<height>[0-9]+)/', 
+        r'(?P<width>[0-9]+)/(?P<height>[0-9]+)', 
         ImageView.as_view(), 
         name=get_name(ImageView)
     ),
