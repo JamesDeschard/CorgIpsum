cdc3d3599abf81403f7216c423fdf71338ae8cfe
James Deschard <65614717+JamesDeschard@users.noreply.github.com>
2022-01-05 12:49:44 +0100

    Update settings.py

--- corgipsum/settings.py
+++ corgipsum/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b^sgm*jbpumv-1zir0mya2*ag^1yrgi+muhb_6(=tqc28%-z=*'
+SECRET_KEY = "secret_ker"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
