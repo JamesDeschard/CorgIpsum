26dfa53ec894ba1ff9155b357938370c636da7f7
James Deschard <james.deschard@gmail.com>
2022-10-15 15:20:54 +0200

    deploy

--- corgipsum/settings.py
+++ corgipsum/settings.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import django_heroku
 import environ
 
+from django.core.management.utils import get_random_secret_key
+
 env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -16,19 +18,19 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY',  default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-if not DEBUG:       
-    CSRF_COOKIE_SECURE = True 
-    SESSION_COOKIE_SECURE = True 
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True  
-    SECURE_HSTS_SECONDS = 86400 
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
+# if not DEBUG:       
+#     CSRF_COOKIE_SECURE = True 
+#     SESSION_COOKIE_SECURE = True 
+#     SECURE_BROWSER_XSS_FILTER = True
+#     SECURE_CONTENT_TYPE_NOSNIFF = True  
+#     SECURE_HSTS_SECONDS = 86400 
+#     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+#     SECURE_HSTS_PRELOAD = True
 
 ALLOWED_HOSTS =  ['127.0.0.1', '.herokuapp.com', 'www.corgipsum.com', 'corgipsum.com']
 
