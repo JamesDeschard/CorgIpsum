371199e8bd822f6007becf141b912b4ca16c1e20
James Deschard <james.deschard@gmail.com>
2022-10-15 14:48:19 +0200

    deploy

--- corgipsum/settings.py
+++ corgipsum/settings.py
@@ -15,6 +15,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
